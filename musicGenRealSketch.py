1a33d6a5030e6d365ca62e329abec2ce2bbfdf28
Jack <36423334+ShadowRogue1987@users.noreply.github.com>
2022-03-20 11:31:38 +0100

    Extra udpate

--- musicGenRealSketch.py
+++ musicGenRealSketch.py
@@ -1,10 +1,8 @@
-import collections
 import datetime
 import fluidsynth
 import glob
 import numpy as np
 import pathlib
-import pandas as pd
 import pretty_midi
 import seaborn as sns
 import tensorflow as tf
@@ -21,27 +19,23 @@ class notenClass:
         self.nootInterval = nootInterval
         self.nootTijd = nootTijd
 
-# NOG AANPASSEN OVER HET INSTRUMENT
-
 # Zet MIDI bestand om in een array van noten met hun bijhorende eigenschappen (bijvoorbeeld: toonhoogte).
 def midiNaarNoten(bestand):
     noten = notenClass([], [], [], [], [])
-
-    # Piano: elk muziekstuk in de dataset wordt gespeeld met de piano.
-    instrument = pretty_midi.PrettyMIDI(bestand).instruments[0]
+    piano = pretty_midi.PrettyMIDI(bestand).instruments[0]
 
     # Rangschik alle gespeelde noten door de piano op chronologische volgorde.
-    notenGerangschikt = sorted(instrument.notes, key = (lambda noot : noot.start))
+    notenGerangschikt = sorted(piano.notes, key = (lambda noot : noot.start))
 
     # Orden alle noten met haar eigenschappen in de noten class.
-    nootStart = notenGerangschikt[0].start
+    beginVorigeNoot = notenGerangschikt[0].start
     for noot in notenGerangschikt:
         noten.toonHoogte.append(noot.pitch)
         noten.beginTijd.append(noot.start)
         noten.eindTijd.append(noot.end)
-        noten.nootInterval.append(noot.start - nootStart)
+        noten.nootInterval.append(noot.start - beginVorigeNoot)
         noten.nootTijd.append(noot.end - noot.start)
-        nootStart = noot.start
+        beginVorigeNoot = noot.start
   
     # Creeër een array die alle noten met haar eigenschappen bevat.
     notenArray = np.stack((noten.toonHoogte, noten.beginTijd, noten.eindTijd, noten.nootInterval, noten.nootTijd), axis = 1)
@@ -57,3 +51,14 @@ for i in glob.glob('data/maestro-v2.0.0/*/*[.mid]*'):
     bestanden.append(i)
 
 bestanden = glob.glob(str(directory/'**/*.midi*'))
+
+aantalBestanden = 5
+totaleNoten = []
+
+# Orden alle noten met haar eigenschappen van het gebruikte aantal muziekstukken in een array.
+for bestand in bestanden[:aantalBestanden]:
+  noten = midiNaarNoten(bestand)
+  totaleNoten.append(noten)
+
+all_notes = np.concatenate(totaleNoten)
+
