9e06f61fd2867b045707386d1803beee38fd15dc
Jack <36423334+ShadowRogue1987@users.noreply.github.com>
2022-03-20 19:14:01 +0100

    grote update

--- musicGenRealSketch.py
+++ musicGenRealSketch.py
@@ -19,6 +19,11 @@ class notenClass:
         self.nootInterval = nootInterval
         self.nootTijd = nootTijd
 
+# Normalizeer de toonhoogte van een noot.
+def normalizeerToonHoogte(notenArray, maxToonHoogte):
+    notenArray = notenArray/[maxToonHoogte,1.0,1.0]
+    return notenArray
+
 # Zet MIDI bestand om in een array van noten met hun bijhorende eigenschappen (bijvoorbeeld: toonhoogte).
 def midiNaarNoten(bestand):
     noten = notenClass([], [], [], [], [])
@@ -42,6 +47,57 @@ def midiNaarNoten(bestand):
     
     return notenArray
 
+# Deelt dataset op in batches met labels en genormalizeerde inputs.
+def verwerkDataset(dataset, batchInputGrootte, maxToonHoogte = 128):
+    batchInputGrootte += 1
+
+    # Maak een nieuwe dataset die windows bevat van de dataset uit de input.
+    windows = dataset.window(batchInputGrootte, shift=1,
+                                drop_remainder=True)
+
+    # Zet de windows om in batches en 'flatten' deze batches tot een nieuwe dataset.
+    flatten = lambda x: x.batch(batchInputGrootte, drop_remainder=True)
+    flattenedDataset = windows.flat_map(flatten)
+
+    # Deel batch op in inputs en een bijbehorend label. En normalizeer de inputs. 
+    def deelNormalizeerBatches(notenArray):
+        inputs = notenArray[:-1]
+        labels = notenArray[-1]
+        labelsDictionary = {}
+        labelsDictionary.update({'toonHoogte' : labels[0]})
+        labelsDictionary.update({'nootInterval' : labels[1]})
+        labelsDictionary.update({'nootTijd' : labels[2]})
+
+        return normalizeerToonHoogte(inputs, maxToonHoogte), labelsDictionary
+
+    # Pas bovenstaande functie toe tot elke batch in de dataset.
+    # 2e argument in Dataset.map(), laat deze bovenstaande functie parallel toegepast worden door CPU.
+    notenDataset = flattenedDataset.map(deelNormalizeerBatches, num_parallel_calls=tf.data.AUTOTUNE)
+
+    return notenDataset
+
+# Zet een array van noten met haar bijhorende eigenschappen om in een MIDI bestand.
+def notenNaarMidi(noten, exportBestand, instrumentNaam = 'Acoustic Grand Piano', geluidsterkte = 100):
+    bestand = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program(instrumentNaam))
+
+    # Zet de array van noten om in Pretty_MIDI noten en voeg deze toe aan het gegeven instrument.
+    startVorigeNoot = 0
+    for noot in noten:
+        nootInterval = noot[3]
+        nootTijd = noot[4]
+        beginTijd = float(startVorigeNoot + nootInterval)
+        eindTijd = float(beginTijd + nootTijd)
+        noot = pretty_midi.Note(velocity = geluidsterkte, pitch = int(noot[0]), start = beginTijd, end = eindTijd)
+        instrument.notes.append(noot)
+        startVorigeNoot = beginTijd
+
+    bestand.instruments.append(instrument)
+
+    # Exporteer de Pretty_MIDI object naar een .midi/.mid bestand in de directory van dit programma.
+    bestand.write(exportBestand)
+    return bestand
+
 # Directory van de dataset.
 directory = pathlib.Path('data/maestro-v2.0.0')
 
@@ -53,12 +109,21 @@ for i in glob.glob('data/maestro-v2.0.0/*/*[.mid]*'):
 bestanden = glob.glob(str(directory/'**/*.midi*'))
 
 aantalBestanden = 5
-totaleNoten = []
+alleNoten = []
 
 # Orden alle noten met haar eigenschappen van het gebruikte aantal muziekstukken in een array.
 for bestand in bestanden[:aantalBestanden]:
-  noten = midiNaarNoten(bestand)
-  totaleNoten.append(noten)
+    noten = midiNaarNoten(bestand)
+    alleNoten.append(noten)
 
-all_notes = np.concatenate(totaleNoten)
+nAlleNoten = len(alleNoten)
+notenArray = np.concatenate(alleNoten)
 
+# Verwijder de begin- en eindtijden voor alle noten.
+trainingsNoten = np.delete(notenArray, [1,2], 1)
+
+# Zet de array van trainingsnoten om in een dataset voor Tensorflow.
+notenDataset = tf.data.Dataset.from_tensor_slices(trainingsNoten)
+
+batchInputGrootte = 25
+verwerkteDataset = verwerkDataset(notenDataset, batchInputGrootte)
